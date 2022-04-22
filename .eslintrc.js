bab6eef485df6ee1c001488e48819f9db92c5611
Matteo Bilotta <mbilotta@linkeurope.it>
2022-04-22 18:11:16 +0200

    [FIX] Replaced Vite.js with `@vercel/ncc`.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
-  extends: ["@byloth/eslint-config-typescript"]
+  extends: ["@byloth/eslint-config-typescript"],
+  ignorePatterns: [
+    "dist",
+    "lib",
+    "node_modules"
+  ]
 };
